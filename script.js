9b348f579a5e7612728ee6aca131c6013d640f32
Praneeshg6 <gonepraneesh6@gmail.com>
2025-09-08 14:50:02 +0530

    initial

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
